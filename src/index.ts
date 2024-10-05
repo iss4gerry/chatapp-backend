d892a7f92a2e8fc26b19a294230c167a5e20ba21
gerry <gerrydesrian488@gmail.com>
2024-10-05 19:44:09 +0700

    update

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import type { Request, Response } from 'express';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello World');
+});
+
+app.listen(3000, () => {
+	console.log('Server is running on port 3000');
+});
