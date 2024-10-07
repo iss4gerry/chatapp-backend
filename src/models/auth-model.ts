2b7870a34202977f66eb33e1576d03fc7fa60b95
gerry <gerrydesrian488@gmail.com>
2024-10-07 12:44:56 +0700

    auth model

--- src/models/auth-model.ts
+++ src/models/auth-model.ts
@@ -2,12 +2,14 @@ import { User } from '@prisma/client';
 
 export type RegisterRequest = {
 	email: string;
+	username: string;
 	name: string;
 	password: string;
 };
 
 export type Response = {
 	id: string;
+	username: string;
 	name: string;
 	email: string;
 };
@@ -21,6 +23,7 @@ export function toUserResponse(user: User): Response {
 	return {
 		id: user.id,
 		name: user.name,
+		username: user.username,
 		email: user.email!,
 	};
 }
